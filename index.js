3b61565ea601a63f49f52352d7ad3ef6208842da
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-01-15 14:59:48 +0200

    Add eslint-config-iamnapo

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const cbm = require('./scripts/cbm-sherlock');
 
 console.log('pure-sherlock returned:', pure());
 (async () => {
-	try {
-		const res = await cbm();
-		console.log(`cbm-sherlock returned: ${res}`);
-	} catch (error) {
-		console.error(`cbm-sherlock returned: ${error}`);
-	}
+  try {
+    const res = await cbm();
+    console.log(`cbm-sherlock returned: ${res}`);
+  } catch (error) {
+    console.error(`cbm-sherlock returned: ${error}`);
+  }
 })();
